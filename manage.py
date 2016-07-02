af3e5bb11ed5dd6e27d1abb8cd834bc71b1b8b8a
Jason Paidoussi <jason@paidoussi.net>
2016-07-02 17:32:46 +0200

    Made development the default settings file for manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mote.settings.production")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "mote.settings.development"
+    )
 
     from django.core.management import execute_from_command_line
 
