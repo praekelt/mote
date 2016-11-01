c6dc4f86f7d65090d0585d4de28221f9f9b91737
cblignaut <silly.blignaut@gmail.com>
2016-11-01 14:12:34 +0200

    Changed upload script to include repo

--- upload_docs.sh
+++ upload_docs.sh
@@ -6,7 +6,7 @@ if [ "X$TRAVIS_BRANCH" == "Xdevelop" -o "X$1" == "Xforce" ]; then
     pip install devpi-client sphinx sphinx_rtd_theme
     devpi use "https://travis:$PW@pypi.praekelt.com/root/praekelt"
     devpi login travis --password="$PW"
-    devpi upload --no-vcs --only-docs
+    devpi upload --no-vcs --formats sdist --with-docs
 else
     echo "Upload skipped, \$TRAVIS_BRANCH is not 'develop'. Or add 'force' parameter."
 fi
