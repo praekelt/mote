8546232a40a455b1e69be19573c1b52144bebe6b
Rijk Stofberg <rijk@praekeltconsulting.com>
2017-02-14 13:25:21 +0200

    Handle the cases where 'source', 'delta' or both are None.

--- mote/utils.py
+++ mote/utils.py
@@ -3,6 +3,9 @@ from copy import deepcopy
 
 def _deepmerge(source, delta):
     """Recursive helper"""
+    
+    if source is None: return delta
+    if delta is None: return source
 
     for key, value in delta.items():
 
