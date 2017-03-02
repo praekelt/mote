469222ddfb3def0b35ab079336e2bc64d6b2cb9e
hedleyroos <hedleyroos@gmail.com>
2017-03-02 18:21:28 +0200

    Get layers to work. Vastly simplify models.

--- mote/tests/test_layers.py
+++ mote/tests/test_layers.py
@@ -52,3 +52,19 @@ class LayersTestCase(TestCase):
             result, "Panel"
         )
 
+    def test_metadata_button(self):
+        """We override button element and data"""
+        obj = get_object_by_dotted_name("myprojectchild.website.atoms.button")
+        self.assertEqual(obj.metadata["title"], "MyProjectChild Button")
+
+    def test_metadata_anchor(self):
+        """We override anchor element but not data"""
+        obj = get_object_by_dotted_name("myprojectchild.website.atoms.anchor")
+        self.assertEqual(obj.metadata["title"], "Anchor")
+
+    def test_metadata_panel(self):
+        """We inherit panel fully"""
+        obj = get_object_by_dotted_name("myprojectchild.website.atoms.panel")
+        self.assertEqual(obj.metadata["title"], "Panel")
+
+
