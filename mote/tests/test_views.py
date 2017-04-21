d52209b1eb2550ade852ea286951a4613cbbf884
Phala Ramonyai <phalamag@gmail.com>
2017-04-21 11:29:50 +0200

    PEP8 fixes

--- mote/tests/test_views.py
+++ mote/tests/test_views.py
@@ -16,13 +16,22 @@ class ViewsTestCase(TestCase):
         cls.factory = RequestFactory()
 
     def test_element_partial(self):
-        url = reverse("mote:element-partial", args=("myproject", "website", "atoms", "button"))
+        url = reverse(
+            "mote:element-partial",
+            args=("myproject", "website", "atoms", "button")
+        )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_element_partial_post_request(self):
         request = self.factory.post(
-            reverse("mote:element-partial", args=("myproject", "website", "atoms", "button"))
+            reverse(
+                "mote:element-partial",
+                args=("myproject", "website", "atoms", "button")
+            )
         )
-        response = views.ElementPartialView.as_view()(request, project="myproject", aspect="website", pattern="atoms", element="button")
+        response = views.ElementPartialView.as_view()(
+            request, project="myproject", aspect="website",
+            pattern="atoms", element="button"
+        )
         self.assertEqual(response.status_code, 200)
