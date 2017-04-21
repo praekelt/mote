fc5501e83384cae9afc3a6bede6c1e9a53cccd68
Phala Ramonyai <phalamag@gmail.com>
2017-04-21 11:25:49 +0200

    Add post method to VariationPartialView and ElementPartialView

--- mote/tests/test_views.py
+++ mote/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import reverse
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
-from mote import models
+from mote import models, views
 
 
 class ViewsTestCase(TestCase):
@@ -13,8 +13,16 @@ class ViewsTestCase(TestCase):
         cls.aspect = models.Aspect("website", cls.project)
         cls.pattern = models.Pattern("atoms", cls.aspect)
         cls.element = models.Element("button", cls.pattern)
+        cls.factory = RequestFactory()
 
     def test_element_partial(self):
         url = reverse("mote:element-partial", args=("myproject", "website", "atoms", "button"))
         response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_element_partial_post_request(self):
+        request = self.factory.post(
+            reverse("mote:element-partial", args=("myproject", "website", "atoms", "button"))
+        )
+        response = views.ElementPartialView.as_view()(request, project="myproject", aspect="website", pattern="atoms", element="button")
         self.assertEqual(response.status_code, 200)
