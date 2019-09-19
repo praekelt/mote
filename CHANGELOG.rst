8e63573ffb9c6fc3bcceaaa5b8602b94e5a90fd9
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-09-19 14:26:35 +0200

    update version and change log

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.1.5
+0.1.6
 -----
 #. First release.
 #. This package is a fork of mote-prk see https://github.com/praekelt/mote
--- mote/__init__.py
+++ mote/__init__.py
@@ -1,7 +1,7 @@
 import threading
 
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 default_app_config = "mote.apps.MoteConfig"
 PROJECT_PATHS = {}
 _thread_locals = threading.local()
